e33a78ff48311211df089d56446556713397ff58
mhsnabz <mhsnabz@yahoo.com.tr>
2020-07-15 05:06:46 +0300

    no msg

--- app/src/main/java/com/gargisoft/carbon/Dialog/WaitDialog.java
+++ app/src/main/java/com/gargisoft/carbon/Dialog/WaitDialog.java
@@ -1,7 +1,26 @@
 package com.gargisoft.carbon.Dialog;
 
+import android.app.Activity;
+import android.app.Dialog;
+
+import com.gargisoft.carbon.R;
+
 public class WaitDialog {
 
-    public WaitDialog() {
+    Dialog dialog;
+    Activity activity;
+
+    public WaitDialog(Activity activity) {
+        this.activity = activity;
+        dialog = new Dialog(activity);
+    }
+
+   public void showWaitDialog(){
+        dialog.setContentView(R.layout.wait_dialog);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
+    }
+    public void dissmisWaitDialog(){
+        dialog.dismiss();
     }
 }
--- app/src/main/java/com/gargisoft/carbon/LoginRegister/LoginActivity.java
+++ app/src/main/java/com/gargisoft/carbon/LoginRegister/LoginActivity.java
@@ -2,22 +2,26 @@ package com.gargisoft.carbon.LoginRegister;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.gargisoft.carbon.Dialog.WaitDialog;
 import com.gargisoft.carbon.Helper.Utils;
 import com.gargisoft.carbon.R;
 
 import okhttp3.internal.Util;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Dialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        WaitDialog dialog = new WaitDialog(this);
+        dialog.showWaitDialog();
     }
 
     public void signUp(View view)
@@ -27,5 +31,13 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(i);
         Utils.go(LoginActivity.this);
 
+    }
+    public void showWaitDialog(){
+        dialog.setContentView(R.layout.wait_dialog);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
+    }
+    public void dissmisWaitDialog(){
+        dialog.dismiss();
     }
 }
