369d5dcf0aa2274d0953ac7fd9af6f4b6adeb4cd
mhsnabz <mhsnabz@yahoo.com.tr>
2020-07-15 09:13:41 +0300

    no msg

--- app/src/main/java/com/gargisoft/carbon/Home/HomeActivity.java
+++ app/src/main/java/com/gargisoft/carbon/Home/HomeActivity.java
@@ -5,22 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.gargisoft.carbon.Model.currentUser;
 import com.gargisoft.carbon.R;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class HomeActivity extends AppCompatActivity {
     currentUser currentUser = new currentUser();
+    CircleImageView profileImage ;
+    TextView username ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        profileImage = (CircleImageView)findViewById(R.id.profileImage);
+        username = (TextView)findViewById(R.id.username);
+
         Bundle extras = getIntent().getExtras();
         Intent intentIncoming = getIntent();
         if (extras != null) {
 
             currentUser = (currentUser) intentIncoming.getParcelableExtra("currentUser");
+            Picasso.get().load(currentUser.getProfileImage()).resize(200,200)
+                    .into(profileImage, new Callback() {
+                        @Override
+                        public void onSuccess() {
 
+                        }
+
+                        @Override
+                        public void onError(Exception e) {
+
+                        }
+                    });
+            username.setText(currentUser.getUsername());
         }
+    }
+
+    private void setUserInfo(){
+
     }
 }
--- app/src/main/java/com/gargisoft/carbon/SplashScreen.java
+++ app/src/main/java/com/gargisoft/carbon/SplashScreen.java
@@ -1,5 +1,6 @@
 package com.gargisoft.carbon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat;
 
@@ -9,10 +10,21 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+import com.gargisoft.carbon.Helper.Utils;
+import com.gargisoft.carbon.Home.HomeActivity;
 import com.gargisoft.carbon.LoginRegister.LoginActivity;
+import com.gargisoft.carbon.Model.currentUser;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SplashScreen extends AppCompatActivity {
     ImageView logo;
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    String currentUser = auth.getUid();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,9 +41,28 @@ public class SplashScreen extends AppCompatActivity {
                 }catch (InterruptedException ex){
                     ex.printStackTrace();
                 }finally {
-                    Intent i = new Intent(SplashScreen.this, LoginActivity.class);
-                    startActivity(i);
-                    finish();
+                    if (currentUser!=null){
+                        DocumentReference ref = FirebaseFirestore.getInstance().collection("user").document(currentUser);
+                        ref.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                if (task.isSuccessful()){
+                                    Intent i = new Intent(SplashScreen.this, HomeActivity.class);
+                                    i.putExtra("currentUser",task.getResult().toObject(com.gargisoft.carbon.Model.currentUser.class));
+                                    startActivity(i);
+                                    Utils.go(SplashScreen.this);
+                                    finish();
+                                }
+                            }
+                        });
+
+                    }else {
+                        Intent i = new Intent(SplashScreen.this, LoginActivity.class);
+                        startActivity(i);
+                        Utils.go(SplashScreen.this);
+                        finish();
+                    }
+
                 }
             }
         };
