e975ce171da384a1aab3f44f010b108ee6a138af
mhsnabz <mhsnabz@yahoo.com.tr>
2020-07-16 15:35:44 +0300

    no msg

--- app/src/main/java/com/gargisoft/carbon/ChatController/ChatActivity.java
+++ app/src/main/java/com/gargisoft/carbon/ChatController/ChatActivity.java
@@ -3,8 +3,10 @@ package com.gargisoft.carbon.ChatController;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.transition.Slide;
 import android.transition.Transition;
 import android.transition.TransitionManager;
@@ -28,13 +30,13 @@ import com.squareup.picasso.Picasso;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ChatActivity extends AppCompatActivity {
-    private Boolean isShow = false;
+    private Boolean isShow = false, isRecording = false;
     currentUser currentUser = new currentUser();
     currentUser otherUser = new currentUser();
-    LottieAnimationView video , image , location , sound ,cancel;
+    LottieAnimationView video , image , location , sound ,cancel , recordBtn  , recording;
     CircleImageView profileImage;
     TextView name ;
-    ImageButton back;
+    ImageButton back , dismiss;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,13 +66,81 @@ public class ChatActivity extends AppCompatActivity {
             name.setText(otherUser.getName());
         }
 
+        ///TODO: record bar init
+
+        recordBtn = (LottieAnimationView)findViewById(R.id.record);
+        recording =(LottieAnimationView)findViewById(R.id.recordinProgres);
+        dismiss = (ImageButton)findViewById(R.id.down);
+
+        recordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isRecording){
 
 
+                    recordBtn.setSpeed(1F);
+                    recordBtn.playAnimation();
+                    recording.setSpeed(0.1334F);
+                    recording.playAnimation();
+                    isRecording = true;
+                }else{
+
+                    recordBtn.setSpeed(-1F);
+                    recordBtn.playAnimation();
+                    recording.pauseAnimation();
+                    isRecording = false;
+
+                }
+            }
+        });
+
+        dismiss.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleRecord(false);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        toggle(true);
+                    }
+                }, 100);
+
+            }
+        });
+
+        recordBtn.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation)
+            {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+
+        ///TODO: main bar ini
         video =(LottieAnimationView)findViewById(R.id.video);
         image =(LottieAnimationView)findViewById(R.id.image);
         sound =(LottieAnimationView)findViewById(R.id.sound);
         location =(LottieAnimationView)findViewById(R.id.location);
         cancel =(LottieAnimationView)findViewById(R.id.cancel);
+
+
+
         video.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -101,24 +171,16 @@ public class ChatActivity extends AppCompatActivity {
         sound.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(ChatActivity.this,"sound",Toast.LENGTH_SHORT).show();
                 toggle(false);
-                Thread thread = new Thread(){
-                    @Override
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
                     public void run() {
-                        super.run();
-                        try{
-                            sleep(1000);
-                        }catch (InterruptedException ex){
-
-                        }finally {
-
-                        }
+                        toggleRecord(true);
                     }
-                };
-                thread.run();
-               // toggleRecord(true);
-                toggleRecord(true);
+                }, 100);
+
+
+
             }
         });
 
@@ -142,7 +204,7 @@ public class ChatActivity extends AppCompatActivity {
         CardView toolbar = (CardView)findViewById(R.id.mediaToolbar);
         ViewGroup parent = findViewById(R.id.parent);
         Transition transition = new Slide(Gravity.BOTTOM);
-        transition.setDuration(1000);
+        transition.setDuration(500);
         transition.addTarget(R.id.mediaToolbar);
 
         TransitionManager.beginDelayedTransition(parent, transition);
@@ -158,14 +220,13 @@ public class ChatActivity extends AppCompatActivity {
         location.playAnimation();
         cancel.playAnimation();
     }
-    private void toggleRecord(boolean show) {
+    private void toggleRecord(final boolean show) {
         CardView toolbar = (CardView)findViewById(R.id.recordBar);
         ViewGroup parent = findViewById(R.id.parent);
         Transition transition = new Slide(Gravity.BOTTOM);
-        transition.setDuration(1000);
+        transition.setDuration(500);
         transition.addTarget(R.id.recordBar);
         TransitionManager.beginDelayedTransition(parent, transition);
         toolbar.setVisibility(show ? View.VISIBLE : View.GONE);
-
     }
 }
